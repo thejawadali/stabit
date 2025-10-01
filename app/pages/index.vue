cba885193b5b079f4035973715f379a22073ec1b
Jawad Ali <jawad.ali@repairdesk.co>
2025-10-01 23:04:31 +0500

    removed testing code

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,5 @@
 <template>
   <div className="min-h-screen">
-    <h1>Color mode: {{ $colorMode.value }}</h1>
-    <select v-model="$colorMode.preference">
-      <option value="system">System</option>
-      <option value="light">Light</option>
-      <option value="dark">Dark</option>
-    </select>
     <LandingPageNavigation />
     <LandingPageHero />
     <LandingPageFeatures />
