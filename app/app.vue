463e73ec40d3c7e4a47a7bdd5270ac55ffad98e9
Jawad Ali <jawad.ali@repairdesk.co>
2025-10-01 12:15:46 +0500

    designed signup page

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline text-primary-500">
-      Hello world!
-    </h1>
-    <Button>Click me</Button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline text-primary-500">Landing page</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
