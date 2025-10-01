fa5f2cffeecd82727f0d3b9830a84844f2a3b752
Jawad Ali <jawad.ali@repairdesk.co>
2025-10-01 09:58:14 +0500

    chore: installed tailwindcss and shadcn library

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline text-primary-500">
+      Hello world!
+    </h1>
+    <Button>Click me</Button>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+  css: ['~/assets/css/index.css'],
   modules: [
+    '@nuxtjs/tailwindcss',
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/image',
-    '@nuxt/test-utils'
-  ]
+    '@nuxt/test-utils',
+    'shadcn-nuxt'
+  ],
+  shadcn: {
+    prefix: '',
+    componentDir: './app/components/ui'
+  }
 })
