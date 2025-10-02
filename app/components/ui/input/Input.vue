1f1d4cbf1614ac9f175460212d14deb4409226cd
Jawad Ali <jawad.ali@repairdesk.co>
2025-10-02 22:53:55 +0500

    added validtion to input field base component

--- app/components/ui/input/Input.vue
+++ app/components/ui/input/Input.vue
@@ -1,32 +1,52 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from "vue"
-import { useVModel } from "@vueuse/core"
 import { cn } from "@/lib/utils"
 
-const props = defineProps<{
-  defaultValue?: string | number
-  modelValue?: string | number
+const props = withDefaults(defineProps<{
   class?: HTMLAttributes["class"],
   placeholder?: string,
   name?: string,
   label?: string,
   rules?: string,
   customErrorMessage?: string,
-}>()
+  validateOnBlur?: boolean
+}>(), {
+  placeholder: '',
+  name: '',
+  label: '',
+  rules: '',
+  customErrorMessage: '',
+  validateOnBlur: false
+})
 
-const emits = defineEmits<{
-  (e: "update:modelValue", payload: string | number): void
-}>()
+const { value, errorMessage, validate, handleReset } = useField(props.name || "", props.rules || "")
 
-const modelValue = useVModel(props, "modelValue", emits, {
-  passive: true,
-  defaultValue: props.defaultValue,
+const modelValue = defineModel<string | number>({ default: '' })
+
+// change value when modelValue changes
+syncRefs(modelValue, value)
+
+
+onMounted(() => {
+  if (!modelValue.value) {
+    setTimeout(handleReset)
+  }
+})
+
+
+const inputClasses = computed(() => {
+  const baseClasses = 'flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 mt-1'
+  const borderClass = errorMessage.value ? 'border-destructive' : 'border-input focus-visible:ring-1 focus-visible:ring-ring'
+  return cn(baseClasses, borderClass, props.class)
 })
 </script>
 
 <template>
-  <div class="space-y-2">
-    <Label :for="props.name">{{ props.label }}</Label>
-    <input v-model="modelValue" :class="cn('flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50', props.class)" :placeholder="props.placeholder">
+  <div class="">
+    <Label :for="props.name">{{ props.label }} <span v-if="rules.includes('required')"
+        class="text-destructive">*</span></Label>
+    <input :id="props.name" v-model="modelValue" :class="inputClasses" :placeholder="props.placeholder"
+      @blur="validateOnBlur ? validate() : null">
+    <p v-if="errorMessage" class="text-sm text-destructive">{{ customErrorMessage || errorMessage }}</p>
   </div>
-  </template>
+</template>
