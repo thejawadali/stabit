ab3e2c23394d0614cc2aea0c4addd0e77c2a8581
Jawad Ali <jawad.ali@repairdesk.co>
2025-10-02 22:54:27 +0500

    defined basic rules

--- app/plugins/rules.ts
+++ app/plugins/rules.ts
@@ -1,6 +1,6 @@
 import { defineRule, configure } from 'vee-validate'
 import * as rules from '@vee-validate/rules'
-import { required, email, min } from '@vee-validate/rules';
+import { required, email, min, max } from '@vee-validate/rules';
 
 
 
@@ -8,21 +8,10 @@ export default defineNuxtPlugin({
   name: 'my-plugin',
   enforce: 'pre', // or 'post'
   async setup() {
-    // Register all the rules globally
+    // Register the rules globally
     defineRule('required', required)
     defineRule('email', email)
     defineRule('min', min)
-    // Object.entries(rules).forEach(([rule, validator]) => {
-    //   defineRule(rule, validator as any)
-    // })
-
-    // configure({
-    //   generateMessage: (ctx) => {
-    //     const messages = {
-    //       required: `${ctx.field} is required.`,
-    //     } as Record<string, string>
-    //     return messages[ctx.rule?.name || ''] || `${ctx.field} is invalid.`
-    //   },
-    // })
+    defineRule('max', max)
   }
 })
