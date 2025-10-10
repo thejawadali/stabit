99a8ee4f7893e8494b00d3d0c45f0fe2db0d4de0
Jawad Ali <jawad.ali@repairdesk.co>
2025-10-10 16:37:44 +0500

    bind input attrs

--- app/components/ui/input/Input.vue
+++ app/components/ui/input/Input.vue
@@ -40,6 +40,10 @@ const showError = computed(() => {
   return resetHasDone && errorMessage.value
 })
 
+defineOptions({
+  inheritAttrs: false
+})
+
 
 const inputClasses = computed(() => {
   const baseClasses = 'flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 mt-1'
@@ -53,9 +57,9 @@ const inputClasses = computed(() => {
     <Label :for="props.name">{{ props.label }} <span v-if="rules.includes('required')"
         class="text-destructive">*</span></Label>
     <div class="flex items-center gap-2">
-      <input :id="props.name" v-model="modelValue" :class="inputClasses" :placeholder="props.placeholder"
+      <input :id="props.name" v-model="modelValue" :class="inputClasses" :placeholder="props.placeholder" v-bind="$attrs"
         @blur="validateOnBlur ? validate() : null">
-        <slot name="suffix" />
+      <slot name="suffix" />
     </div>
     <p v-if="showError" class="text-sm text-destructive">{{ customErrorMessage || errorMessage }}</p>
   </div>
