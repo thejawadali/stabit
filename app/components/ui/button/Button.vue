4e3f881f0eb7190fe6a973a5da6bcc424ee4fa7e
Jawad Ali <jawad.ali@repairdesk.co>
2025-10-14 13:17:56 +0500

    button loading state

--- app/components/ui/button/Button.vue
+++ app/components/ui/button/Button.vue
@@ -10,10 +10,12 @@ interface Props extends PrimitiveProps {
   variant?: ButtonVariants["variant"]
   size?: ButtonVariants["size"]
   class?: HTMLAttributes["class"]
+  isLoading?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
   as: "button",
+  isLoading: false,
 })
 </script>
 
@@ -22,7 +24,9 @@ const props = withDefaults(defineProps<Props>(), {
     :as="as"
     :as-child="asChild"
     :class="cn(buttonVariants({ variant, size }), props.class)"
+    :disabled="isLoading || disabled"
   >
+    <IconLoader2 v-if="isLoading" class="w-4 h-4 mr-2 animate-spin" />
     <slot />
   </Primitive>
 </template>
