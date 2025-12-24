6c8d77be2060879bf76ab7a825dfdc4448febc89
Jawad Ali <jawad.ali@repairdesk.co>
2025-12-24 18:17:25 +0500

    fix: wrong paths for prisma import

--- server/api/habit-logs/index.get.ts
+++ server/api/habit-logs/index.get.ts
@@ -1,4 +1,4 @@
-import prisma from '../../../lib/prisma'
+import { prisma } from '../../utils/prisma'
 import { serverSupabaseUser } from '#supabase/server'
 
 export default defineEventHandler(async (event) => {
